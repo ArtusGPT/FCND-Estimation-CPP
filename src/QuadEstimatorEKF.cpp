773d130b07122b8ce1be6d1788e6670af9a2d97f
ArtusGPT <expertminter30@gmail.com>
2026-06-16 14:11:51 +0200

    Scenario 07: quaternion attitude estimation in UpdateFromIMU

--- src/QuadEstimatorEKF.cpp
+++ src/QuadEstimatorEKF.cpp
@@ -93,13 +93,24 @@ void QuadEstimatorEKF::UpdateFromIMU(V3F accel, V3F gyro)
   // (replace the code below)
   // make sure you comment it out when you add your own code -- otherwise e.g. you might integrate yaw twice
 
-  float predictedPitch = pitchEst + dtIMU * gyro.y;
-  float predictedRoll = rollEst + dtIMU * gyro.x;
-  ekfState(6) = ekfState(6) + dtIMU * gyro.z;	// yaw
+    Quaternion<float> q = Quaternion<float>::FromEuler123_RPY(rollEst, pitchEst, ekfState(6));
+    q.IntegrateBodyRate(gyro, dtIMU);
+    float predictedRoll = q.Roll();
+    float predictedPitch = q.Pitch();
+    ekfState(6) = q.Yaw();
+
+	if (ekfState(6) > F_PI) ekfState(6) -= 2.f * F_PI;
+	if (ekfState(6) < -F_PI) ekfState(6) += 2.f * F_PI;
+
+  // commented out code begins here
+  // float predictedPitch = pitchEst + dtIMU * gyro.y;
+  // float predictedRoll = rollEst + dtIMU * gyro.x;
+  // ekfState(6) = ekfState(6) + dtIMU * gyro.z;	// yaw
 
   // normalize yaw to -pi .. pi
-  if (ekfState(6) > F_PI) ekfState(6) -= 2.f*F_PI;
-  if (ekfState(6) < -F_PI) ekfState(6) += 2.f*F_PI;
+  // if (ekfState(6) > F_PI) ekfState(6) -= 2.f*F_PI;
+  // if (ekfState(6) < -F_PI) ekfState(6) += 2.f*F_PI;
+  // commented out code ends here
 
   /////////////////////////////// END STUDENT CODE ////////////////////////////
 
